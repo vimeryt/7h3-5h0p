049a8ea8570971c4fcb669c45730cff5a6570389
unknown <olga.ms.o@yandex.ru>
2017-10-27 18:14:46 +0300

    try N2

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const browserSync = require('browser-sync');
 const autoprefixer = require('gulp-autoprefixer');
+const rename       = require('gulp-rename');
+const ejs          = require('gulp-ejs');
+const gutil        = require('gulp-util');
 
 // Автоперезагрузка при изменении файлов в папке `dist`:
 // Принцип: меняем файлы в `/src`, они обрабатываются и переносятся в `dist` и срабатывает автоперезагрузка.
@@ -37,7 +40,9 @@ gulp.task('js', () => {
 });
 
 gulp.task('html', () => {
-    gulp.src('src/index.html')
+    gulp.src('src/index.ejs')
+    .pipe(ejs().on('error', gutil.log))
+    .pipe(rename('index.html'))
         .pipe(gulp.dest('./dist'));
 });
 
@@ -45,6 +50,7 @@ gulp.task('html', () => {
 gulp.task('watch', () => {
     gulp.watch('src/less/**/*.less', ['styles']);
     gulp.watch('src/**/*.html', ['html']);
+    gulp.watch('src/**/*.ejs', ['html']);
     gulp.watch('src/img/**/*.*', ['img']);
     gulp.watch('src/js/**/*.*', ['js']);
 });
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,41 +1,41 @@
 // Your code here...
 (function () {
-    var controls = document.getElementsByClassName("color__item");
-    for (var i = 0; i < controls.length; i++) {
-        var control = controls[i];
+    const controls = document.getElementsByClassName('color__item');
+    for (let i = 0; i < controls.length; i++) {
+        const control = controls[i];
         control.addEventListener('click',function(event) {
             event.preventDefault();
 
-            for(var i = 0; i < controls.length; i++) {
+            for(let i = 0; i < controls.length; i++) {
                 if (controls[i].classList.contains('color__item_active')) {
                     controls[i].classList.remove('color__item_active');
                 }
             }
-            event.target.className += " color__item_active";
+            event.target.className += ' color__item_active';
 
-            var imgs = document.getElementsByClassName("product__img");
-            for (i = 0; i < imgs.length; i++) {
-                imgs[i].style.display = 'none';
+            const images = document.getElementsByClassName('product__img');
+            for (let i = 0; i < images.length; i++) {
+                images[i].style.display = 'none';
             }
-            var img_id = event.target.getAttribute("data-color");
+            const img_id = event.target.getAttribute('data-color');
             document.getElementById(img_id).style.display = 'block';
         });
     }
 })();
 
 (function () {
-    var controls = document.getElementsByClassName("size__item");
-    for (var i = 0; i < controls.length; i++) {
-        var control = controls[i];
+    const controls = document.getElementsByClassName('size__item');
+    for (let i = 0; i < controls.length; i++) {
+        const control = controls[i];
         control.addEventListener('click',function(event) {
             event.preventDefault();
 
-            for (var i = 0; i < controls.length; i++) {
+            for (let i = 0; i < controls.length; i++) {
                 if (controls[i].classList.contains('size__item_active')) {
                     controls[i].classList.remove('size__item_active');
                 }
             }
-            event.target.className += " size__item_active";
+            event.target.className += ' size__item_active';
         });
     }
 })();
