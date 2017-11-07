1d79a518334e7279bc821cad63bdc523c7861dcd
unknown <olga.ms.o@yandex.ru>
2017-11-07 15:39:00 +0300

    made changes according to review & add image compression

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,41 +1,42 @@
-// Your code here...
 (function () {
-    const controls = document.getElementsByClassName('color__item');
-    for (let i = 0; i < controls.length; i++) {
-        const control = controls[i];
-        control.addEventListener('click',function(event) {
+    const colors = document.getElementsByClassName('color__item');
+    const sizes = document.getElementsByClassName('size__item');
+    const selectors = [...colors, ...sizes];
+
+    for (let i = 0; i < selectors.length; i++) {
+        const selector = selectors[i];
+
+        selector.addEventListener('click',function(event) {
             event.preventDefault();
 
-            for(let i = 0; i < controls.length; i++) {
-                if (controls[i].classList.contains('color__item_active')) {
-                    controls[i].classList.remove('color__item_active');
+            const isColorSelector = !!selector.dataset.color;
+
+
+
+            for(let i = 0; i < selectors.length; i++) {
+                if (isColorSelector) {
+                    if (selectors[i].classList.contains('color__item_active')) {
+                        selectors[i].classList.remove('color__item_active');
+                    }
+                    event.target.className += ' color__item_active';
+                } else {
+                    if (selectors[i].classList.contains('size__item_active')) {
+                        selectors[i].classList.remove('size__item_active');
+                    }
+                    event.target.className += ' size__item_active';
                 }
+
             }
-            event.target.className += ' color__item_active';
 
-            const images = document.getElementsByClassName('product__img');
-            for (let i = 0; i < images.length; i++) {
-                images[i].style.display = 'none';
-            }
-            const img_id = event.target.getAttribute('data-color');
-            document.getElementById(img_id).style.display = 'block';
-        });
-    }
-})();
 
-(function () {
-    const controls = document.getElementsByClassName('size__item');
-    for (let i = 0; i < controls.length; i++) {
-        const control = controls[i];
-        control.addEventListener('click',function(event) {
-            event.preventDefault();
-
-            for (let i = 0; i < controls.length; i++) {
-                if (controls[i].classList.contains('size__item_active')) {
-                    controls[i].classList.remove('size__item_active');
+            if (isColorSelector) {
+                const images = document.getElementsByClassName('product__img');
+                for (let i = 0; i < images.length; i++) {
+                    images[i].style.display = 'none';
                 }
+                const img_id = event.target.getAttribute('data-color');
+                document.getElementById(img_id).style.display = 'block';
             }
-            event.target.className += ' size__item_active';
         });
     }
 })();
